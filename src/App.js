1eb17aec826ccf03cc469f0ab79c8482241abc36
rifinote <82169430+rifinote@users.noreply.github.com>
2022-03-10 19:52:59 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bem Vindo ao meu Primeiro React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/rifinote/react-joaopaulo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Meu GitHub
         </a>
       </header>
     </div>
